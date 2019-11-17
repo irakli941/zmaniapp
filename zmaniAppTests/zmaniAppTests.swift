bc2e46065c7b7ab942940221d00f5809c859977b
Irakli Shelia <i.shelia@bog.ge>
2019-11-17 16:32:53 +0400

    minor changes

--- zmaniAppTests/zmaniAppTests.swift
+++ zmaniAppTests/zmaniAppTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import zmani
 
-class DonnaAppTests: XCTestCase {
+class zmaniAppTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
